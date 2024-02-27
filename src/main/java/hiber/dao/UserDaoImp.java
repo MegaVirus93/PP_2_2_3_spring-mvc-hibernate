280e71cdb2832fc9d304649a72bb1aa1ee636408
MegaVirus93 <MegaVirus93@gmail.com>
2024-02-27 17:30:17 +0300

    2.3.1. Spring MVC + Hibernate. [преподготовка-снимок]

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,39 @@
+package hiber.dao;
+
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public UserDaoImp(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Override
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User", User.class);
+        return query.getResultList();
+    }
+
+//    @Override
+//    public User getUserByCar(String model) {
+//        return sessionFactory.getCurrentSession()
+//                .createQuery("FROM User WHERE userCar.model = :model", User.class)
+//                .setParameter("model", model).uniqueResult();
+//    }
+}
---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,25 @@
+package web.controller;
+
+import hiber.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "/")
+    public String printUsers(Model model) {
+        model.addAttribute("user_list", userService.listUsers());
+        return "user_view";
+    }
+
+}
